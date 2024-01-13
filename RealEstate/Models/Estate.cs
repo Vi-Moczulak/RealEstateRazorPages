0db0459b7e933f0e5ac88387e69a868d8da5242b
Vi-Moczulak <wmoczulak00@gmail.com>
2024-01-13 13:59:10 +0100

    Add project files.

---
+++ RealEstate/Models/Estate.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
+{
+    public class Estate
+    {
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+
+        [Required]
+        public int BedRooms { get; set; }
+
+        [Required]
+        public int BathRooms { get; set; }
+
+        [Required]
+        public int SquareFeet { get; set; }
+
+        public int? TypeId { get; set; }
+
+        public Type? Type { get; set; }
+
+
+    }
+}
---
+++ RealEstate/Models/Type.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RealEstate.Models
+{
+    public class Type
+    {
+        public int Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(50)]
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<Estate>? Estates { get; set; }
+    }
+}
